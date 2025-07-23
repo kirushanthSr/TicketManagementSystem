8cb2fddaac1367ea2e285747d56944b0a9551c12
kirushanthSr <kirushanth0611@gmail.com>
2025-07-23 13:40:37 +0530

    File update

--- TicketManagementSystem/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module RealTimeEventTicketingApplication {
-    requires com.google.gson;
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.graphics;
-    exports View;
-    exports Model;
-}
